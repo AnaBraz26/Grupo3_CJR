18a99d0c3be69ea034ab91b2d65d5e47f13b9346
GSTV-AVL <gusavila0317@gmail.com>
2024-05-15 14:44:57 -0300

    Adicionado: Front de entrada de Email na pagina de login

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,18 @@
+"use client"; // top to the file
 import Image from "next/image";
-import {Formik, Form, Field} from "formik"
+import {Formik, Form, Field} from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("email inválido inserido").required("campo obrigatório"),
+  password: Yup.string().required("campo obrigatório")
+});
+
+const initualValues = { email: "", password: ""}
+
+const submitionHandler = (values: any) => {
+  console.log(values);
+}
 
 export default function Home() {
   return (
@@ -12,6 +25,18 @@ export default function Home() {
                 <div className="w-1/2 flex">
                   <div className="m-auto">
                     <h2 className="w-96 h-16 text-center text-5x1 font-bold">Avaliação de Professores</h2>
+                    <Formik initialValues={initualValues}  validationSchema={validationSchema} onSubmit={submitionHandler}>
+                      <Form className="flex flex-col">
+                        <div className="mb-4">
+                          <Field 
+                          type = "email" 
+                          name = "email" 
+                          placeholder = "Email" 
+                          id = "email"
+                          />
+                        </div>
+                      </Form>
+                    </Formik>
                   </div>
                 </div>
               </div>              
