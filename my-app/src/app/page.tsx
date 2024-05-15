ff0fc02f8688776458a340f2502f7276de3d6f64
GSTV-AVL <gusavila0317@gmail.com>
2024-05-14 22:46:27 -0300

    Melhora: melhorei a divisão da pagina de login

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
           <div className="flex items center justify win-h screen bg-gray-100">
-            <img className="h-auto max-w-lg" src="image 1.png" alt="image description"/>
-            <div>
-    
+            <div className="w-full bg-white shadow-md ">
+              <div className="Flex h-full">
+                <div className="w-1/2 bg-gray-100">
+                  <img className="w-full h-full" src="image 1.png" alt="image description"/>
+                </div>
+              </div>              
             </div>
           </div>
       );
