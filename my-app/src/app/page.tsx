d566900d495f1fb8148e498a23b0408858e6c867
GSTV-AVL <gusavila0317@gmail.com>
2024-05-15 00:25:37 -0300

    Mudança: desenvolvimento inicial da metade de login da tela

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-          <div className="flex items center justify win-h screen bg-gray-100">
-            <div className="w-full bg-white shadow-md ">
-              <div className="Flex h-full">
-                <div className="w-1/2 bg-gray-100">
-                  <img className="w-full h-full" src="image 1.png" alt="image description"/>
+          <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+              <div className="flex h-full">
+                <div className="w-1/2 bg-gray-200">
+                  <img className="w-full h-full" src="image 1.png" alt="Erro ao carregar imagem!"/>
+                </div>
+                <div className="w-1/2 flex">
+                  <div className="m-auto">
+                    <h2 className="w-96 h-16 text-center text-5x1 font-bold">Avaliação de Professores</h2>
+                  </div>
                 </div>
               </div>              
             </div>
