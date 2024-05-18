e5bad0f6dd368d286622cda24c175e0d64b6fa9f
Ana Caroline <212008482@aluno.unb.br>
2024-05-18 00:15:49 -0300

    feat & refactor: página do feed e mudança na navbar

--- my-app/src/app/Navbar.tsx
+++ my-app/src/app/Navbar.tsx
@@ -2,8 +2,16 @@ export default function NavBar() {
     return (
         <div className="flex justify-between items-center h-99px bg-[#A4FED3]">
             <div className="pl-8">
-                <img className="w-92px cursor-pointer" src="unbLogo.png" alt="Logo da UnB" />
-            </div>            
+                <a href = "https://cjr.org.br/">
+                <img className="w-92px cursor-pointer" src="cjr.png" alt="Logo da UnB" />
+                </a>
+            </div>   
+
+            <div className="pr-96">
+                <a href = "https://www.unb.br/">
+                <img className="w-92px cursor-pointer" src="unbLogo.png" alt="Logo da UNB" />
+                </a>
+            </div>           
            
             <a href = "http://localhost:3000/">
             <button className="w-154px h-14 rounded-16px border-2 border-white mr-14 bg-[#00ABED] shadow-custom duration-500 hover:duration-500 hover:bg-[#0077B5]" >
@@ -11,7 +19,7 @@ export default function NavBar() {
             </button>  
             </a>
 
-            <div className="flex items-center justify-round pr-9">
+            {/* <div className="flex items-center justify-round pr-9">
                 <img className="cursor-pointer w-45px h-45px mr-74px" src="notification.png" alt="Icone para olhar notificação" />
                 <a href = "http://localhost:3000/profile">
                 <img className="cursor-pointer rounded-100px mr-19px" src="morty.jpg" alt="Foto de pergil" />
@@ -19,7 +27,7 @@ export default function NavBar() {
                 <a href="http://localhost:3000/">
                 <img className="cursor-pointer" src="exit.svg" alt="Icone de saída" />
                 </a>
-            </div>            
+            </div>             */}
         </div>
     );
 }
