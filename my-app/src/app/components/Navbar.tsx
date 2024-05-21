1c1be2be24d5e7c2863e20081a86cd73fb769cf1
Ana Caroline <212008482@aluno.unb.br>
2024-05-21 01:02:26 -0300

    Feat & Ref: Finalizando a pág do Feed e arrumando arquivos

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -1,18 +1,18 @@
 export default function NavBar() {
     return (
-        <div className="flex justify-between items-center h-99px bg-[#A4FED3]">
-            <div className="pl-8">
-                <a href = "https://cjr.org.br/">
-                <img className="w-92px cursor-pointer" src="cjr.png" alt="Logo da UnB" />
-                </a>
-            </div>   
-
-            <div className="pr-96">
+        <div className="flex justify-between items-center h-99px bg-[#A4FED3] ">
+            <div className="flex-wrap ml-14">
                 <a href = "https://www.unb.br/">
                 <img className="w-92px cursor-pointer" src="unbLogo.png" alt="Logo da UNB" />
                 </a>
-            </div>           
-           
+            </div>  
+
+            <div className="pl-8 flex-wrap">
+                <a href = "https://novositecjr.vercel.app/">
+                <img className="w-92px cursor-pointer" src="cjr.png" alt="Logo da UnB" />
+                </a>
+            </div>     
+
             <a href = "http://localhost:3000/">
             <button className="w-154px h-14 rounded-16px border-2 border-white mr-14 bg-[#00ABED] shadow-custom duration-500 hover:duration-500 hover:bg-[#0077B5]" >
                 <h1 className="font-normal text-30px font-questrial text-white text-center">Login</h1>
