50226698f91ed15b65082abb6eb97496a8b81f95
Ana Caroline <212008482@aluno.unb.br>
2024-05-27 21:33:20 -0300

    Merge branch 'main' into Carol

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -7,27 +7,27 @@ export default function NavBar() {
                 </a>
             </div>  
 
-            <div className="pl-8 flex-wrap">
+            {/* <div className="pl-8 flex-wrap">
                 <a href = "https://novositecjr.vercel.app/">
                 <img className="w-92px cursor-pointer" src="cjr.png" alt="Logo da UnB" />
                 </a>
-            </div>     
+            </div>   */}   
 
-            <a href = "http://localhost:3000/">
+            {/*<a href = "http://localhost:3000/">
             <button className="w-154px h-14 rounded-16px border-2 border-white mr-14 bg-[#00ABED] shadow-custom duration-500 hover:duration-500 hover:bg-[#0077B5]" >
                 <h1 className="font-normal text-30px font-questrial text-white text-center">Login</h1>
             </button>  
-            </a>
+            </a>*/}
 
-            {/* <div className="flex items-center justify-round pr-9">
+            <div className="flex items-center justify-round pr-9">
                 <img className="cursor-pointer w-45px h-45px mr-74px" src="notification.png" alt="Icone para olhar notificação" />
                 <a href = "http://localhost:3000/profile">
-                <img className="cursor-pointer rounded-100px mr-19px" src="morty.jpg" alt="Foto de pergil" />
+                <img className="cursor-pointer rounded-100px mr-19px w-64px" src="morty.jpg" alt="Foto de pergil" />
                 </a>
                 <a href="http://localhost:3000/">
                 <img className="cursor-pointer" src="exit.svg" alt="Icone de saída" />
                 </a>
-            </div>             */}
+            </div>
         </div>
     );
 }
