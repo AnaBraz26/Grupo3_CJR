b3f5f5dfbc3c77f2cf24a0d4378f3cdb1d0a90df
GSTV-AVL <gusavila0317@gmail.com>
2024-06-13 18:02:49 -0300

    Fix: professorID to number 3

--- project-new/src/discipline/dto/update-discipline.dto.ts
+++ project-new/src/discipline/dto/update-discipline.dto.ts
@@ -1,11 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDisciplineDto } from './create-discipline.dto';
-import { IsDate, IsString } from 'class-validator';
+import { IsDate, IsInt, IsString } from 'class-validator';
 
 export class UpdateDisciplineDto extends PartialType(CreateDisciplineDto) {
     @IsString()
     name?: string;
-    @IsString()
+    @IsInt()
     professorId?: number;
     @IsDate()
     creatAt?: Date;
