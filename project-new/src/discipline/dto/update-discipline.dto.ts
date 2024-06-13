6743bd48e917b5d225b68a69782f333cb4af86f7
AlanHenriqueRF <alhenrangelfera@gmail.com>
2024-06-13 18:31:41 -0300

    :bug: fix: Was fixed the dto of disciplines

--- project-new/src/discipline/dto/update-discipline.dto.ts
+++ project-new/src/discipline/dto/update-discipline.dto.ts
@@ -1,12 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDisciplineDto } from './create-discipline.dto';
-import { IsDate, IsString } from 'class-validator';
+import { IsDate, IsInt, IsString } from 'class-validator';
 
 export class UpdateDisciplineDto extends PartialType(CreateDisciplineDto) {
     @IsString()
     name?: string;
-    @IsString()
-    professorId?: string;
+    @IsInt()
+    professorId?: number;
     @IsDate()
     creatAt?: Date;
     @IsDate()
