4d13f30f8f2c49ee7051467ec6dd8645c1da7b94
GSTV-AVL <gusavila0317@gmail.com>
2024-06-14 03:17:25 -0300

    Feature: Requisição em modal comentário

--- my-app/src/app/modal/Comentario.tsx
+++ my-app/src/app/modal/Comentario.tsx
@@ -1,19 +1,55 @@
-import React from "react";
+import React, { useState} from "react";
+import { Formik, Form, Field } from "formik";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import * as Yup from "yup";
+import { Content } from "next/font/google";
 
 const ReactQuill = dynamic(() => import('react-quill'), {ssr: false})
 
-interface ModalProps{
+interface Modal_commentProps{
     isVisible: boolean;
     onClose: () => void;
 }
 
-const Modal_comentar: React.FC<ModalProps> = ({isVisible, onClose}) =>{
+const validationSchema = Yup.object({
+    content: Yup.string().required("campo obrigatório"),
+  });
+
+const initualValues = { content: "" }
+
+function Modal_comentar({onClose}: Modal_commentProps){
+
+    const [content, setContent] = useState("");
+    const [conflitError, setConflitError] = useState(false);
+    const router = useRouter()
+
+
+
+const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const body = { content: content }
+
+    axios.post("http://localhost:2000/comments", body)
+        .then(() => {
+          console.log("deu bom")
+          router.push('/')
+  
+        })
+        .catch((err) => {
+          console.log(err)
+          })
+  
+};
+
+/*const Modal_comentar: React.FC<ModalProps> = ({isVisible, onClose}) =>{
     const [editorContent, setEditorContent] = React.useState<string>('');
 
-    if(!isVisible) return null;
+    if(!isVisible) return null;  */
 
+const [editorContent, setEditorContent] = React.useState<string>('');
     return(
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
             <div className="flex flex-col m-10 items-center bg-green-300 w-1/2 h-3/4 p-5 rounded-xl">
