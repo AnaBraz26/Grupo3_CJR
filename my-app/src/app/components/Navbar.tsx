4bb91ea22dc71caa365e522ff2fc8cd310cfa1e0
AlanHenriqueRF <alhenrangelfera@gmail.com>
2024-06-16 10:01:51 -0300

    :sparkles: feat: Navbar working with the the token of each user

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -1,37 +1,37 @@
+import { useRouter } from "next/navigation";
+
 export default function NavBar() {
+    const token = localStorage.getItem("token")
+    const UserId = localStorage.getItem("UserId")
+    const router = useRouter()
+
+    function deslogar(){
+        localStorage.clear();
+        router.push('/')
+    }
+
     return (
         <div className="flex justify-between items-center h-99px bg-[#A4FED3] ">
             <div className="flex-wrap ml-14">
-                <a href = "https://www.unb.br/">
-                <img className="w-92px cursor-pointer" src="unbLogo.png" alt="Logo da UNB" />
+                <a href="https://www.unb.br/">
+                    <img className="w-92px cursor-pointer" src="unbLogo.png" alt="Logo da UNB" />
                 </a>
-            </div>  
-
-            {/* <div className="pl-8 flex-wrap">
-                <a href = "https://novositecjr.vercel.app/">
-                <img className="w-92px cursor-pointer" src="cjr.png" alt="Logo da UnB" />
-                </a>
-            </div>   */}   
-
-            {/* if(!localStorage.getItem("user"))
-            {
-                <a href = "http://localhost:3000/">
-                <button className="w-154px h-14 rounded-16px border-2 border-white mr-14 bg-[#00ABED] shadow-custom duration-500 hover:duration-500 hover:bg-[#0077B5]" >
-                    <h1 className="font-normal text-30px font-questrial text-white text-center">Login</h1>
-                </button>  
-                </a>
-            }
-            else{ */}
+            </div>
+            {token ?
                 <div className="flex items-center justify-round pr-9">
                     <img className="cursor-pointer w-45px h-45px mr-74px" src="notification.png" alt="Icone para olhar notificação" />
-                    <a href = "http://localhost:3000/profile">
-                    <img className="cursor-pointer rounded-100px mr-19px w-64px" src="morty.jpg" alt="Foto de pergil" />
+                    <a href="http://localhost:3000/profile">
+                        <img className="cursor-pointer rounded-100px mr-19px w-64px" src="morty.jpg" alt="Foto de pergil" />
                     </a>
-                    <a href="http://localhost:3000/">
-                    <img className="cursor-pointer" src="exit.svg" alt="Icone de saída" />
-                    </a>
-                </div>
-            
+                        <img onClick={()=>deslogar()} className="cursor-pointer" src="exit.svg" alt="Icone de saída" />
+                </div> :
+                <a href="http://localhost:3000/">
+                    <button className="w-154px h-14 rounded-16px border-2 border-white mr-14 bg-[#00ABED] shadow-custom duration-500 hover:duration-500 hover:bg-[#0077B5]" >
+                        <h1 className="font-normal text-30px font-questrial text-white text-center">Login</h1>
+                    </button>
+                </a>}
+
+
         </div>
     );
 }
